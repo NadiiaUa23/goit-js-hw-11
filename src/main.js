857c548b14703bf5f6986a9c762a10164dc20444
NadiiaUa23 <hasiminka92@gmail.com>
2024-05-20 22:51:06 +0200

    add файли

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 //У файлі main.js напиши всю логіку роботи додатка.
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
