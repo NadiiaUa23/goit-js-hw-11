387f1fca6f2c6edacc5ec77535bb90ff38ea3b0d
NadiiaUa23 <hasiminka92@gmail.com>
2024-05-22 20:54:31 +0200

    add import

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,14 @@
 //У файлі pixabay-api.js зберігай функції для HTTP-запитів
+const apiKey = '43983774-8711aa48aacb0ae1050be5e44';
+
+
+fetch("<https://pixabay.com/api/?key=${apiKey}&q=q=${searchQuery}>")
+  .then(response => {
+    // Response handling
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
